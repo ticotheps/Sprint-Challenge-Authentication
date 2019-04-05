2f3444bed9a91395920d85e02e103b850a18efb2
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-04-05 11:41:34 -0400

    Creates: a 'register()' function, a 'login()' function, and a 'generateToken()' function (that takes in 'user' as an argument) in the routes.js file

--- config/routes.js
+++ config/routes.js
@@ -14,10 +14,40 @@ module.exports = server => {
 
 function register(req, res) {
   // implement user registration
-}
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 10);
+  user.password = hash;
+
+  Users.add(user)
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+};
 
 function login(req, res) {
   // implement user login
+  let { username, password } = req.body;
+
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
+
+        res.status(200).json({  
+          message: `Welcome ${user.username}! You now have a token!`,
+          token,
+        });
+      } else {
+        res.status(401).json({ message: 'Invalid credentials' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
 }
 
 function getJokes(req, res) {
@@ -33,4 +63,17 @@ function getJokes(req, res) {
     .catch(err => {
       res.status(500).json({ message: 'Error Fetching Jokes', error: err });
     });
+}
+
+function generateToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username,
+  };
+
+  const options = {
+    expiresIn: '1d',
+  };
+
+  return jwt.sign(payload, secret, options);
 }
